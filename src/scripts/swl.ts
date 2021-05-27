f80aea934a6d620d9ff85422f608590e8d20dfe6
Christophe Eymard <christophe.eymard@sales-way.com>
2021-05-27 15:45:26 +0200

    swl executable for easier command line building

--- src/scripts/swl.ts
+++ src/scripts/swl.ts
@@ -1,5 +1,8 @@
 #!/usr/bin/env -S node --enable-source-maps
 
+import { extname, join } from "path"
+import { execSync } from "child_process"
+
 class AliasMap {
   map = new Map<string, {source: string | null, sink: string | null}>()
 
@@ -10,29 +13,55 @@ class AliasMap {
 }
 
 let alias = new AliasMap()
-  .add("swl-pg-src", "swl-pg-sink", "pg")
-  .add("swl-sqlite-src", "swl-sqlite-sink", "sqlite")
-  .add("swl-xls-src", "swl-xls-sink", "xls")
-  .add(null, "swl-flatten", "flatten")
-  .add(null, "swl-unflatten", "unflatten")
-  .add(null, "swl-coerce", "coerce")
-  .add(null, "swl-uncoerce", "uncoerce")
+  .add(_("pg-src"), _("pg-sink"), "pg")
+  .add(_("sqlite-src"), _("sqlite-sink"), "sqlite")
+  .add(_("xlsx-src"), _("xlsx-sink"), "xls", "xlsx")
+  .add(_("yaml-src"), _("yaml-sink"), "yaml", "yml")
+  .add(null, _("flatten"), "flatten")
+  .add(null, _("unflatten"), "unflatten")
+  .add(null, _("coerce"), "coerce")
+  .add(null, _("uncoerce"), "uncoerce")
 
 let file_extensions = new AliasMap()
-  .add("swl-sqlite-src", "swl-sqlite-sink", ".db", ".sqlite")
-  .add("swl-xls-src", "swl-xls-sink", ".xlsx", ".ods", ".xlsb", ".xls", '.xlsm')
+  .add(_("sqlite-src"), _("sqlite-sink"), ".db", ".sqlite")
+  .add(_("xlsx-src"), _("xlsx-sink"), ".xlsx", ".ods", ".xlsb", ".xls", '.xlsm')
+  .add(_("yaml-src"), _("yaml-sink"), ".yaml", ".yml")
 
-let prototols = new AliasMap()
-  .add("swl-pg-src", "swl-pg-sink", "postgres://")
+let protocols = new AliasMap()
+  .add(_("pg-src"), _("pg-sink"), "postgres://")
 
 
-console.log(get_commands())
+get_commands().then(c => {
+  let res = execSync(c, { stdio: "inherit" })
+  // console.log(c)
+})
 
 //////////////////////////////////////////////////////////////////////
 //// Utility functions
 //////////////////////////////////////////////////////////////////////
 
-function get_commands() {
+/**
+ * Figure out which sink or source should be used for the current item.
+ * @param item The current argument item
+ */
+async function figure_out_who(item: string, source: boolean): Promise<string[]> {
+
+  // First figure out if it was named in its alias
+  let alv = alias.map.get(item)
+  if (alv != null) {
+    return ["node", "--enable-source-maps", source? alv.source : alv.sink]
+  }
+
+  let ext = extname(item)
+  alv = file_extensions.map.get(ext)
+  if (alv != null) {
+    return ["node", "--enable-source-maps", source ? alv.source : alv.sink, item]
+  }
+
+  return [item]
+}
+
+async function get_commands() {
   let commands: {command: string[], source: boolean}[] = []
   {
     let command: string[] = []
@@ -49,5 +78,25 @@ function get_commands() {
       }
     }
   }
-  return commands
+
+  let builder: string[] = []
+  let first = true
+
+  for (let c of commands) {
+    if (!first) { builder.push("|") } else { first = false }
+    let res = c.command.slice(1)
+    let item = await figure_out_who(c.command[0], c.source)
+    c.command = [...item, ...res]
+    builder = [...builder, ...c.command]
+  }
+
+  return builder.map(b => {
+    if (b.includes(" "))
+      b = `"${b.replace("\"", "\\\"")}"`
+    return b
+  }).join(" ")
+}
+
+function _(name: string) {
+  return join(__dirname, `swl-${name}.js`)
 }
