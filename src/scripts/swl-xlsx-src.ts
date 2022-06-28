acf510c5a11e4a42f145e93393199b844592d380
Christophe Eymard <christophe.eymard@sales-way.com>
2022-06-28 14:51:03 +0200

    better xlsx handling

--- src/scripts/swl-xlsx-src.ts
+++ src/scripts/swl-xlsx-src.ts
@@ -2,8 +2,10 @@
 
 import { source, emit } from "../index"
 import { arg, oneof, optparser, param } from "../optparse"
+import * as xl from "xlsx-stream-reader"
 
-import { readFile } from "xlsx"
+// import { readFile } from "xlsx"
+import * as fs from "fs"
 
 let collections_opts = optparser(
   arg("name").required(),
@@ -27,85 +29,114 @@ let opts = {
 
 if (!opts.file) throw new Error("need a file")
 
+let reader = new xl()
+
 source(function () {
 
   // Build a list of known columns
-  const _l = ['', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-  const COLS: string[] = []
+  // const _l = ['', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+  // const COLS: string[] = []
 
-  for (let i = 0; i < _l.length; i++) {
-    for (let j = 1; j < _l.length; j++) {
-      COLS.push(_l[i] + _l[j])
-    }
-  }
+  // for (let i = 0; i < _l.length; i++) {
+  //   for (let j = 1; j < _l.length; j++) {
+  //     COLS.push(_l[i] + _l[j])
+  //   }
+  // }
 
-
-  let wb = readFile(opts.file, { cellHTML: false, cellText: false, cellFormula: false, cellNF: false })
-
-  for (let sname of wb.SheetNames) {
-    const s = wb.Sheets[sname]
-
-    if (opts.sources.size > 0 && !opts.sources.has(sname))
-      continue
-
-    emit.collection(sname)
-
-    const re_range = /^([A-Z]+)(\d+):([A-Z]+)(\d+)$/
-    const match = re_range.exec(s['!ref'] as string)
-    if (!match) continue
-
-    // Try to figure out if we were given a header position globally
-    // or for this specific sheet
-    let header_line = 1
-    let header_column = 0
-
-    // const re_header = /^([A-Z]+)(\d+)$/
-    // const hd = this.options.header || sources && sources![sname]
-    // if (typeof hd === 'string') {
-    //   var m = re_header.exec(hd)
-    //   if (m) {
-    //     header_column = COLS.indexOf(m[1])
-    //     header_line = parseInt(m[2])
-    //   }
-    // }
-    // We have to figure out the number of lines
-    const lines = parseInt(match[4])
-
-    // Then we want to find the header row. By default it should be
-    // "A1", or the first non-empty cell we find
-    const header: string[] = []
-    for (let i = header_column; i < COLS.length; i++) {
-      const cell = s[`${COLS[i]}${header_line}`]
-      if (!cell || !cell.v)
-      break
-      header.push(cell.v)
+  reader.on("worksheet", ws => {
+    if (ws.name) {
+      emit.collection(ws.name)
     }
 
-    // Now that we've got the header, we just go on with the rest of the lines
-    var not_found_count = 0
-    for (var j = header_line + 1; j <= lines; j++) {
-      var obj: {[name: string]: any} = {}
-      var found = false
-      for (let i = header_column; i < header.length; i++) {
-        const cell = s[`${COLS[i]}${j}`]
-        if (cell) {
-          obj[header[i - header_column]] = cell.v
-          found = true
-          not_found_count = 0
-        } else {
-          obj[header[i - header_column]] = null
-        }
+    let columns: string[] = []
+    let len = 0
+
+    ws.on("row", row => {
+      if (row.attributes.r === "1") {
+        columns = row.values
+        len = columns.length
+        return
       }
 
-      if (found)
-        emit.data(obj)
-      else {
-        not_found_count++
-        if (not_found_count > 5)
-        // More than five empty rows in a row means we're at the end of the document.
-          break
+      let values = row.values // array
+      let obj = {}
+      for (let i = 1; i < len; i++) {
+        obj[columns[i]] = values[i]
       }
-    }
+      emit.data(obj)
+    })
 
-  }
+    ws.process()
+  })
+
+  fs.createReadStream(opts.file).pipe(reader)
+
+  // let wb = readFile(opts.file, { cellHTML: false, cellText: false, cellFormula: false, cellNF: false })
+
+  // for (let sname of wb.SheetNames) {
+  //   const s = wb.Sheets[sname]
+
+  //   if (opts.sources.size > 0 && !opts.sources.has(sname))
+  //     continue
+
+  //   emit.collection(sname)
+
+  //   const re_range = /^([A-Z]+)(\d+):([A-Z]+)(\d+)$/
+  //   const match = re_range.exec(s['!ref'] as string)
+  //   if (!match) continue
+
+  //   // Try to figure out if we were given a header position globally
+  //   // or for this specific sheet
+  //   let header_line = 1
+  //   let header_column = 0
+
+  //   // const re_header = /^([A-Z]+)(\d+)$/
+  //   // const hd = this.options.header || sources && sources![sname]
+  //   // if (typeof hd === 'string') {
+  //   //   var m = re_header.exec(hd)
+  //   //   if (m) {
+  //   //     header_column = COLS.indexOf(m[1])
+  //   //     header_line = parseInt(m[2])
+  //   //   }
+  //   // }
+  //   // We have to figure out the number of lines
+  //   const lines = parseInt(match[4])
+
+  //   // Then we want to find the header row. By default it should be
+  //   // "A1", or the first non-empty cell we find
+  //   const header: string[] = []
+  //   for (let i = header_column; i < COLS.length; i++) {
+  //     const cell = s[`${COLS[i]}${header_line}`]
+  //     if (!cell || !cell.v)
+  //     break
+  //     header.push(cell.v)
+  //   }
+
+  //   // Now that we've got the header, we just go on with the rest of the lines
+  //   var not_found_count = 0
+  //   for (var j = header_line + 1; j <= lines; j++) {
+  //     var obj: {[name: string]: any} = {}
+  //     var found = false
+  //     for (let i = header_column; i < header.length; i++) {
+  //       const cell = s[`${COLS[i]}${j}`]
+  //       if (cell) {
+  //         obj[header[i - header_column]] = cell.v
+  //         found = true
+  //         not_found_count = 0
+  //       } else {
+  //         obj[header[i - header_column]] = null
+  //       }
+  //     }
+
+  //     if (found)
+  //       emit.data(obj)
+  //     else {
+  //       not_found_count++
+  //       if (not_found_count > 5)
+  //       // More than five empty rows in a row means we're at the end of the document.
+  //         break
+  //     }
+  //   }
+
+  // }
 })
