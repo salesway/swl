f0da88482f75b28d9c70778adacabaca7e6a9066
Christophe Eymard <christophe.eymard@sales-way.com>
2024-12-11 10:49:49 +0100

    json now uses json5

--- src/scripts/swl-json-src.ts
+++ src/scripts/swl-json-src.ts
@@ -4,21 +4,29 @@ import { readFileSync } from "fs"
 
 import { arg, optparser, param } from "../optparse"
 import { default_opts, emit, source } from '../index'
+import { parse } from "json5"
 
 let opts = optparser(
-  arg("file").required(),
   param("-e", "--encoding").as("encoding"),
   param("-c", "--collection").as("collection"),
+  arg("file").required(),
   default_opts,
 )
   .parse()
 
 source(function () {
-  let contents = readFileSync(opts.file, { encoding: (opts.encoding as BufferEncoding) ?? "utf-8" })
-  let parsed = JSON.parse(contents)
+  const source = opts.file
+  const file_is_json = source[0] === "[" || source[0] === "{"
+
+  let contents = file_is_json ? source : readFileSync(source, { encoding: (opts.encoding as BufferEncoding) ?? "utf-8" })
+
+  let parsed = parse(contents)
+  const default_collection = file_is_json ? "json" : source.replace(/.json$/, "")
 
   if (Array.isArray(parsed)) {
-    parsed = { [opts.collection ?? opts.file.replace(/.json$/, "")]: parsed }
+    parsed = { [opts.collection ?? default_collection]: parsed }
+  } else if (file_is_json) {
+    parsed = { [opts.collection ?? default_collection]: [parsed] }
   }
 
   for (const [col, cts] of Object.entries(parsed)) {
