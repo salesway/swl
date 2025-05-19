1e805b443e62eb1ba24b52301741814abf99ffb0
Christophe Eymard <christophe.eymard@sales-way.com>
2025-05-19 22:14:00 +0200

    json -o

--- scripts/swl-json-sink.ts
+++ scripts/swl-json-sink.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env -S bun run
 
 import { log2, sink, Sink, default_opts, } from "../src/index"
-import { optparser, arg, } from "../src/optparse"
+import { optparser, arg, flag, } from "../src/optparse"
 
 import * as fs from "fs"
 import * as path from "path"
@@ -11,6 +11,7 @@ import * as path from "path"
 let opts_parser = optparser(
   default_opts,
   arg("path").required().help("%.json or collection.json or directory or empty string"),
+  flag("--object", "-o").as("obj").help("output as object of collections instead of array"),
 )
 
 let opts = opts_parser.parse()
@@ -21,32 +22,44 @@ sink((): Sink => {
   if (!opts.path) {
     opts.path = process.cwd()
   }
+
   const stat = fs.statSync(opts.path, { throwIfNoEntry: false })
   const _w = stat?.isDirectory() || opts.path.includes("%") ? null : fs.createWriteStream(opts.path, {  })
+
+
   if (_w) {
-    _w.write("[\n")
+    if (opts.obj) {
+      _w.write("{\n")
+    }
   }
+
   let _first = true
 
   return {
     collection(col, start) {
       const col_path = stat?.isDirectory() ? path.join(opts.path, col.name + ".json") : opts.path.replace(/%/, col.name)
       const w = _w ?? fs.createWriteStream(col_path)
-      let first = _w ? _first : true
+      let first = true
+
+      if (!_first) {
+        w.write(",")
+      } else {
+        _first = false
+      }
+      w.write(opts.obj ? `"${col.name}": [\n` : "[\n")
 
       return {
         data(data: any) {
           if (first) {
             _first = first = false
-            if (_w == null) { w.write("[\n") }
           } else {
             w.write(",\n")
           }
           w.write(JSON.stringify(data))
         },
         end () {
+          w.write("\n]")
           if (_w == null) {
-            w.write("\n]")
             w.close()
           }
         }
@@ -57,7 +70,10 @@ sink((): Sink => {
       log2("error")
     },
     end() {
-      _w?.write("]")
+
+      if (opts.obj) {
+        _w?.write("}")
+      }
       _w?.close()
       log2("Finished")
     },
